b78c4ca3e89b2bd44719be45f67babb51fd583cb
rlaals0203 <scientistmin@gmail.com>
2024-10-03 14:32:56 +0900

    feat : DOTween

---
+++ Assets/01 MemberFolder/KimMin/Script/Player/CameraMove.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private float sensivity;
+
+    private Transform _playerTrm;
+}
