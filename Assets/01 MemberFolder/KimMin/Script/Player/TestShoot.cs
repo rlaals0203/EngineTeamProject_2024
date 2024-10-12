53ca3563be92af27cf3a5d3c891dc692318996a9
rlaals0203 <scientistmin@gmail.com>
2024-10-13 00:17:28 +0900

    feat : Ball Shooting

--- Assets/01 MemberFolder/KimMin/Script/Player/TestShoot.cs
+++ Assets/01 MemberFolder/KimMin/Script/Player/TestShoot.cs
@@ -2,18 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class TestShoot : MonoBehaviour
 {
     [SerializeField] private float _power;
+    Vector3 shootDir;
 
     private Rigidbody _rigid;
-    private Transform _ballCam;
+    private Transform _cam;
+    private Transform _player;
     public event Action OnShoot;
 
     private void Awake()
     {
-        _ballCam = GameObject.Find("BallCamera").transform;
+        _player = GameObject.Find("Player").transform;
+        _cam = GameObject.Find("PlayerCamera").transform;
         _rigid = GetComponent<Rigidbody>();
     }
 
@@ -27,9 +31,19 @@ public class TestShoot : MonoBehaviour
 
     private void Shooting()
     {
-        Vector3 shootDir = (_ballCam.position - transform.position).normalized;
-        _rigid.AddForce(-shootDir * _power, ForceMode.Impulse);
+        Vector3 fixedPos = new Vector3
+            (_cam.position.x, _player.position.y, _cam.position.z);
 
+        Vector3 shootDir = (_player.position - fixedPos).normalized;
+
+        _rigid.AddForce(shootDir * _power, ForceMode.Force);
+
+        Debug.Log(shootDir);
         OnShoot?.Invoke();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(_player.position, shootDir);
     }
 }
