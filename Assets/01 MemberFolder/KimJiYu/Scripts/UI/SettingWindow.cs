bbe5404ece86e50003f498b433ec741e147e7171
Cwldb <mozi0810@gmail.com>
2024-11-19 15:17:50 +0900

    fix : SettingPanel

--- Assets/01 MemberFolder/KimJiYu/Scripts/UI/SettingWindow.cs
+++ Assets/01 MemberFolder/KimJiYu/Scripts/UI/SettingWindow.cs
@@ -2,10 +2,13 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SettingWindow : MonoBehaviour
 {
+    [SerializeField] private GameObject _bgPanel;
+
     private Image _settingPanel;
 
     private bool _isMove = false;
@@ -20,6 +23,7 @@ public class SettingWindow : MonoBehaviour
 
     private void Start()
     {
+        _bgPanel.SetActive(false);
         DOTween.Init();
         _oldPosition = _settingPanel.rectTransform.position.y;
     }
@@ -41,6 +45,7 @@ public class SettingWindow : MonoBehaviour
 
     private void DownPanel()
     {
+        _bgPanel.SetActive(true);
         sequence = DOTween.Sequence();
         sequence.Append(_settingPanel.rectTransform.DOLocalMoveY(0, 1));
         sequence.AppendCallback(() => _isMove = true);
@@ -48,8 +53,26 @@ public class SettingWindow : MonoBehaviour
 
     private void UpPanel()
     {
+        _bgPanel.SetActive(false);
         sequence = DOTween.Sequence();
         sequence.Append(_settingPanel.rectTransform.DOMoveY(_oldPosition, 1));
         sequence.AppendCallback(() => _isMove = false);
+    }
+
+    public void PanelUpButton()
+    {
+        if (_isMove)
+            UpPanel();
+    }
+
+    public void PanelDownButton()
+    {
+        if(!_isMove)
+            DownPanel();
+    }
+
+    public void returnTitle()
+    {
+        SceneManager.LoadScene("TestTitle");
     }
 }
