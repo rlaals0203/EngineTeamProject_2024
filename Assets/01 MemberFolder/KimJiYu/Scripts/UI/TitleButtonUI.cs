e1b8d6daea7f39c016acc91c2d06da9aa334ff4f
Cwldb <mozi0810@gmail.com>
2024-11-25 23:12:58 +0900

    feat : btnAnimation

---
+++ Assets/01 MemberFolder/KimJiYu/Scripts/UI/TitleButtonUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TitleButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Button _btn;
+
+    private Vector3 _oldPosition;
+
+    private void Awake()
+    {
+        _btn = GetComponent<Button>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _btn.transform.DOScale(new Vector3(1.1f, 1.1f, 1.1f),0.1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _btn.transform.DOScale(new Vector3(1f, 1f, 1f), 0.1f);
+    }
+}
